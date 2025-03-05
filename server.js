791815a52b189f996da7bf270aea125f7f5f955e
Amir <101679935+amir-aliyu@users.noreply.github.com>
2025-03-04 20:19:43 -0500

    change server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "test-roomme-app/build")));
+app.use(express.static(path.join(__dirname, "test-roomme-app")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "test-roomme-app/build", "index.html"));
+  res.sendFile(path.join(__dirname, "test-roomme-app", "index.html"));
 });
 
 app.listen(PORT, () => {
